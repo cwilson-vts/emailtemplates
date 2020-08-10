da6ce0926ecfb110393f0172bbafa934c41195c8
cwilson-vts <31514549+cwilson-vts@users.noreply.github.com>
2020-08-10 12:25:38 -0500

    First attempt
    First attempt to get blob to work to make the .eml file i am needing for the app.

--- src/app/misc/ns/ns.component.ts
+++ src/app/misc/ns/ns.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { TeamService } from '../../shared/services/teams.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-ns',
@@ -10,21 +11,21 @@ import { TeamService } from '../../shared/services/teams.service';
 export class NsComponent implements OnInit {
   type: 'sub' | 'notSub';
   form;
-  customerName: string = '';
-  ticket: string = '';
-  reason: string = '';
-  startingNight: string = '';
-  storeClose: string= '';
-  storeEod: string = '';
-  notes: string = '';
+  customerName = '';
+  ticket = '';
+  reason = '';
+  startingNight = '';
+  storeClose = '';
+  storeEod = '';
+  notes = '';
   emailContent;
   emailContentEncoded;
 
-  constructor(private team: TeamService) {}
-  
-teamArry;
-managers;
-leadership;
+  constructor(private team: TeamService, private sanitizer: DomSanitizer) {}
+
+  fileUrl;
+  managers;
+  leadership;
 
   ngOnInit() {
     this.type = 'notSub';
@@ -38,8 +39,7 @@ leadership;
 
   onSubmit(form: NgForm) {
     this.type = 'sub';
-    this.emailContent = 
-    `Store: ${form.value.custName}
+    this.emailContent = `Store: ${form.value.custName}
 
     Task: ${form.value.reason}
 
@@ -50,13 +50,52 @@ leadership;
     EOD Runs At: ${form.value.sEod}
 
     Notes: ${form.value.notes}
-
-    ${this.teamArry.team} - ${this.teamArry.name} - ${this.teamArry.phone}
         `;
 
     this.emailContentEncoded = encodeURIComponent(this.emailContent);
   }
+
   goBack() {
     this.type = 'notSub';
   }
+
+  sendEmail() {
+    const data = `
+    To: User <user@domain.demo>
+Subject: 🔥 Night Shift Appointment for: {{ customerName }} {{ ticket }} 🔥
+X-Unsent: 1
+Content-Type: text/html
+<p>
+    🔥 Night Shift Appointment for: {{ customerName }} {{ ticket }} 🔥
+    <br />
+    Store: {{ customerName }}<br />
+    Task: {{ reason }} <br />
+    Starting the Night of: {{ startingNight }} <br />
+    Store Close @: {{ storeClose }} <br />
+    Store has EOD @: {{ storeEod }} <br />
+    Notes: {{ notes }} <br />
+  </p>
+  <p>Support Leadership <br /></p>
+
+  <div *ngFor="let p of leadership">
+    {{ p.title }} - {{ p.name }} - {{ p.phone }}
+  </div>
+  <br />
+  Team Managers:<br />
+  <div *ngFor="let m of managers">
+    <table>
+      <tr>
+        <td>Team {{ m.team }}</td>
+        <td></td>
+        <td>{{ m.name }}</td>
+        <td></td>
+        <td>{{ m.phone }}</td>
+      </tr>
+    </table>`;
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      window.URL.createObjectURL(blob)
+    );
   }
+}
